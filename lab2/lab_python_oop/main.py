5cbc820ba210ae9af217cb93a371236e2109a86c
Egor Zhidkov <e.jidkov@gmail.com>
2020-12-25 13:27:25 +0300

    Add files via upload

---
+++ lab2/lab_python_oop/main.py
@@ -0,0 +1,11 @@
+from lab_python_oop.Rectangle import Rect
+from lab_python_oop.Square import Square
+from lab_python_oop.Circle import Circle
+
+c = Circle(3, 'зеленый')
+s = Square(2, 'красный')
+v = Rect(5, 4, 'Коричневый')
+
+print(c)
+print(s)
+print(v)
